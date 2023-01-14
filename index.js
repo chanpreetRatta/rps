eef10d78ab3ad46cfda6087615b35bf36b878dc0
Chanpreet@email.com <chanpreetlopon@gmail.com>
2023-01-13 19:29:27 -0800

    add function to play a single game

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ function getComputerChoice() {
   else if (choice === 1) return "paper";
   else if (choice === 2) return "scissor";
 }
+
+function getHumanChoice() {
+  let choice = prompt(
+    "Enter you choice, Rock, Paper or Scissors."
+  ).toLowerCase();
+  return choice;
+}
+
+function playRound() {
+  const computerChoice = getComputerChoice();
+  const humanChoice = getHumanChoice();
+  const winStatement = `You win! ${humanChoice} beats ${computerChoice}`;
+  const drawStatement = `It's Draw! ${humanChoice} is same as ${computerChoice}`;
+  const lostStatement = `You Loss! ${computerChoice} beats ${humanChoice}`;
+
+  if (humanChoice === "paper" && computerChoice === "rock") return winStatement;
+  else if (humanChoice === "rock" && computerChoice === "scissor")
+    return winStatement;
+  else if (humanChoice === "scissor" && computerChoice === "paper")
+    return winStatement;
+  else if (humanChoice === computerChoice) return drawStatement;
+  else return lostStatement;
+}
