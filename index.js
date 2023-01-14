5648096fad1607d24c1890e25f8215123a8848b4
Chanpreet@email.com <chanpreetlopon@gmail.com>
2023-01-13 19:27:36 -0800

    add getComputer Choice function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello");
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) return "rock";
+  else if (choice === 1) return "paper";
+  else if (choice === 2) return "scissor";
+}
