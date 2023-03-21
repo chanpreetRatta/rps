0d7c068fca9b4b9a00bf8520848fd2edc6a0941b
Chanpreet <chanpreetlopon@gmail.com>
2023-03-20 18:48:20 -0700

    fully functional ui

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   if (choice === 0) return "rock";
   else if (choice === 1) return "paper";
-  else if (choice === 2) return "scissor";
+  else if (choice === 2) return "scissors";
 }
 
 // This whole section will get human choice//
 //Start//
 
 const main = document.querySelectorAll(".main > div");
-let RESULT = document.querySelector(".result");
+let RESULT = document.querySelector(".results");
+let scoreBoard = document.querySelectorAll(".results > div ");
+console.log(scoreBoard);
 
-function printResult(result, display) {
-  display.textContent = result;
+function printResult(result, display, winner) {
+  let counter = 0;
+  let finalScore = 0;
+
+  if (winner === 1)
+    display.childNodes.forEach((element) => {
+      if (element.className === "description")
+        console.log((element.textContent = result));
+    });
 }
 
 main.forEach((element) => element.addEventListener("click", playRound));
@@ -22,17 +31,21 @@ function playRound() {
   const computerChoice = getComputerChoice();
   const humanChoice = this.className;
 
+  const computerWins = -1;
+  const draw = 0;
+  const humanWins = 1;
+
   const winStatement = `You win! ${humanChoice} beats ${computerChoice}`;
   const drawStatement = `It's a Draw! ${humanChoice} is same as ${computerChoice}`;
   const lostStatement = `You Loss! ${computerChoice} beats ${humanChoice}`;
 
   if (humanChoice === "paper" && computerChoice === "rock") {
-    printResult(winStatement, RESULT);
-  } else if (humanChoice === "rock" && computerChoice === "scissor") {
-    printResult(winStatement, RESULT);
-  } else if (humanChoice === "scissor" && computerChoice === "paper") {
-    printResult(winStatement, RESULT);
+    printResult(winStatement, RESULT, humanWins);
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    printResult(winStatement, RESULT, humanWins);
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    printResult(winStatement, RESULT, humanWins);
   } else if (humanChoice === computerChoice) {
-    printResult(drawStatement, RESULT);
-  } else printResult(lostStatement, RESULT);
+    printResult(drawStatement, RESULT, draw);
+  } else printResult(lostStatement, RESULT, computerWins);
 }
